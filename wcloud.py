9db1bb413a9605a328d15acfcce97ac7061719b8
Marcus Edel <marcus.edel@fu-berlin.de>
2020-08-06 01:20:16 +0200

    Add initial worldcloud C++ wrapper.

---
+++ wcloud.py
@@ -0,0 +1,10 @@
+from wordcloud import WordCloud, STOPWORDS
+
+def wordcloud(words, filename='output.png', height=2000, width=4000):
+    words = words.replace(';', ' ')
+    word_cloud = WordCloud(stopwords=STOPWORDS, background_color='white', height=height, width=width).generate(words)
+    word_cloud.to_file(filename)
+    return 1
+
+
+wordcloud("the;the;the;one;two")
---
+++ wordcloud.hpp
@@ -0,0 +1,73 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <string>
+
+int WordCloud(const std::string& words, const std::string& filename = "output.png", const int width = 2000, const int height = 4000)
+{
+    PyObject *pName, *pModule, *pFunc;
+    PyObject *pArgs, *pValue;
+    int i;
+
+    Py_Initialize();
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("sys.path.append(\".\")");
+    pName = PyUnicode_DecodeFSDefault("wcloud");
+
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+
+    if (pModule != NULL)
+    {
+        pFunc = PyObject_GetAttrString(pModule, "wordcloud");
+
+        if (pFunc && PyCallable_Check(pFunc))
+        {
+            pArgs = PyTuple_New(4);
+
+            PyObject* pValueWords = PyUnicode_FromString(words.c_str());
+            PyTuple_SetItem(pArgs, 0, pValueWords);
+
+            PyObject* pValueFilename = PyUnicode_FromString(filename.c_str());
+            PyTuple_SetItem(pArgs, 1, pValueFilename);
+
+            PyObject* pValueWidth = PyLong_FromLong(width);
+            PyTuple_SetItem(pArgs, 2, pValueWidth);
+
+            PyObject* pValueHeight = PyLong_FromLong(height);
+            PyTuple_SetItem(pArgs, 3, pValueHeight);
+
+            pValue = PyObject_CallObject(pFunc, pArgs);
+            Py_DECREF(pArgs);
+            if (pValue != NULL)
+            {
+                printf("Result of call: %ld\n", PyLong_AsLong(pValue));
+                Py_DECREF(pValue);
+            }
+            else
+            {
+                Py_DECREF(pFunc);
+                Py_DECREF(pModule);
+                PyErr_Print();
+                fprintf(stderr,"Call failed\n");
+                return 1;
+            }
+        }
+        else
+        {
+            if (PyErr_Occurred())
+                PyErr_Print();
+        }
+        Py_XDECREF(pFunc);
+        Py_DECREF(pModule);
+    }
+    else
+    {
+        PyErr_Print();
+        return 1;
+    }
+    if (Py_FinalizeEx() < 0)
+    {
+        return 120;
+    }
+    return 0;
+}
